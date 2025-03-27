26a13c107d0734b16cf8ecd024495cfda41773c3
mehithka <mehith.karacasia@gmail.com>
2025-03-27 17:32:51 +0500

    a1

---
+++ a1.py
@@ -0,0 +1,9 @@
+#import necessary libraries
+from tkinter import *
+#create window
+window = Tk()
+
+#set the win dow title geometry
+window.title('demo window')
+window.geometry('400x300')
+window.mainloop()
